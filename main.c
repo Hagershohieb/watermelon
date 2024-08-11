ece2347c6c646acb1f594e5b8b5dc0ab853c968e
Hager shohieb <hagershohieb0@gmail.com>
2024-08-11 20:49:13 +0300

    the answer of the problem

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int w=0;
+int main()
+{
+    scanf("%d",&w);
+    if(w%2==0&&w>2)
+     {
+        printf("YES\n");
+     }
+    else
+     {
+        printf("NO\n");
+     }
+    return 0;
+}
